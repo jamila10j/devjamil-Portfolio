7c53565cad07135b73b08f85b2a3f582927a8670
jamila10j <jamilabram5620@gmail.com>
2022-09-24 01:37:26 -0400

    Server Change

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static('devjamil-portfolio' + '/'));
 app.listen(process.env.PORT || 8000);
